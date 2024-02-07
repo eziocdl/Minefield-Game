7528ad924541eab3f110fc052f18374db04edc82
Eziocdl <eziocdl@gmail.com>
2024-02-07 16:28:23 -0300

    final commit in 07/07/2024

---
+++ src/modelo/ResultadoEvento.java
@@ -0,0 +1,19 @@
+package modelo;
+
+public class ResultadoEvento {
+	
+	private final boolean ganhou;
+
+	public boolean isGanhou() {
+		return ganhou;
+	}
+
+	public ResultadoEvento(boolean ganhou) {
+
+		this.ganhou = ganhou;
+	}
+	
+	
+	
+
+}
